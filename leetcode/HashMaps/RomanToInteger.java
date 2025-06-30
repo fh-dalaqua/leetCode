31db838fd7bde86bcb00101895b4f2b8cf0f6188
fh-dalaqua <felphshernandes@gmail.com>
2025-06-30 17:30:13 -0300

    Resolução do exercício 9 LeetCode

--- leetcode/HashMaps/RomanToInteger.java
+++ leetcode/HashMaps/RomanToInteger.java
@@ -1,4 +1,4 @@
-package RomanToInteger;
+package leetcode.HashMaps;
 
 import java.util.HashMap;
 
@@ -25,16 +25,16 @@ public class RomanToInteger {
 
         // percorrendo o HashMap
         for (int i = s.length() - 1; i >= 0; i--) {
-                char c = s.charAt(i);
-                int atual = map.get(c);
+            char c = s.charAt(i);
+            int atual = map.get(c);
 
-                // verificação se é necessário subtrair
-                if(atual >= anterior){
-                    total += atual;
-                } else {
-                    total -= atual;
-                }
-                anterior = atual;
+            // verificação se é necessário subtrair
+            if(atual >= anterior){
+                total += atual;
+            } else {
+                total -= atual;
+            }
+            anterior = atual;
         }
         return total;
     }
